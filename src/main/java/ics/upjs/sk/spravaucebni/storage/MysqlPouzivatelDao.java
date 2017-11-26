8559a11500102b3f3c57928928a8176e6cd1e0ef
Jakub Džama <Jakub Dama@158.197.202.136>
2017-11-26 23:51:20 +0100

    5. commit - implementacia getAll, getById

--- src/main/java/ics/upjs/sk/spravaucebni/storage/MysqlPouzivatelDao.java
+++ src/main/java/ics/upjs/sk/spravaucebni/storage/MysqlPouzivatelDao.java
@@ -1,10 +1,13 @@
 package ics.upjs.sk.spravaucebni.storage;
 
 import ics.upjs.sk.spravaucebni.Pouzivatel;
-import ics.upjs.sk.spravaucebni.storage.PouzivatelDao;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 
 public class MysqlPouzivatelDao implements PouzivatelDao {
 
@@ -16,9 +19,19 @@ public class MysqlPouzivatelDao implements PouzivatelDao {
     
     @Override
     public List<Pouzivatel> getAll() {
-        List<Pouzivatel> pouzivatelia = new ArrayList<>();
-        
-        return pouzivatelia;
+        String sql = "SELECT * FROM pouzivatel";
+        return jdbcTemplate.query(sql, new RowMapper<Pouzivatel>() {
+            @Override
+            public Pouzivatel mapRow(ResultSet rs, int rowNum) throws SQLException {
+                Pouzivatel pouzivatel = new Pouzivatel();
+                pouzivatel.setId(rs.getLong("id"));
+                pouzivatel.setMeno(rs.getString("meno"));
+                pouzivatel.setPoslednePrihlasenie(rs.getTimestamp("posledne_prihlasenie").toLocalDateTime());
+                pouzivatel.setEmail(rs.getString("email"));
+                return pouzivatel;
+            }
+            
+        });
     } 
     
     
